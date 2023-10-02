1959f9e57069de811cbb5ba5f0b2fb81b29958b2
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2023-10-02 17:33:50 +0200

    Move BaseApp to subclass by users in core
    Also add some documentation

---
+++ examples/simple_project.py
@@ -0,0 +1,36 @@
+
+from traitlets import Enum, Float, Unicode
+
+from data_assistant.config import BaseApp
+from data_assistant.config import ConfigurablePlus
+from data_assistant.config.dask_config import DaskClusterPBS, DaskClusterSLURM
+
+
+class Parameters(ConfigurablePlus):
+    # Your parameters definition goes here !
+
+    region = Unicode('GS', help='region')
+    threshold = Float(5., help='threshold for HI')
+    kind = Enum(['1thr', '2thr', '2d'], default_value='2thr',
+                help='kind of histogram')
+
+
+class App(BaseApp):
+    classes = [Parameters, DaskClusterPBS, DaskClusterSLURM]
+    add_to_aliases = [Parameters]
+
+
+if __name__ == '__main__':
+    app = App()
+    # Initialize: this retrieves the configuration values from
+    # config file or command line arguments.
+    app.initialize()
+
+    # Eventually, execute some actions if prompted like printing
+    # the configuration or overwritting the config file.
+    app.start()
+
+    # The configuration can be access in the form of a nested
+    # dictionnary (traitlets.config.ConfigDict)
+    c = app.config
+    print(c.Parameters.region)
--- src/data_assistant/config/core.py
+++ src/data_assistant/config/core.py
@@ -1,12 +1,27 @@
 
-from traitlets.config import Configurable
-from traitlets.config.loader import KVArgParseConfigLoader, Config, DeferredConfig
+from os import path
+import copy
+
+from traitlets.config import Application, Configurable
+from traitlets.config.loader import (
+    KVArgParseConfigLoader, Config, DeferredConfig, _is_section_key
+)
 from traitlets.utils.text import wrap_paragraphs
+from traitlets import Bool, Unicode
 
 
-class ConfigurablePlus(Configurable):
+class AutoConfigurable(Configurable):
+    """Automatically tag its traits as configurable.
+
+    On class definition (using the ``__init_subclass__`` hook) tag
+    all class traits as configurable, unless already tagged as False.
+    Just save you from adding ``...).tag(config=True)`` to every trait.
+
+    Also add a class method to generate the config for a single trait.
+    """
 
     def __init_subclass__(cls, /, **kwargs):
+        """Subclass init hook."""
         super().__init_subclass__(**kwargs)
         # first setup class descriptors
         cls.setup_class(cls.__dict__)
@@ -23,7 +38,7 @@ class ConfigurablePlus(Configurable):
         """Generate config text for a single trait.
 
         Useful if parameters have been added and the configuration
-        file must be changed.
+        file must be changed, but only partially.
         """
         trait = getattr(cls, parameter)
         # Taken from traitlets.Configurable.class_config_section()
@@ -46,6 +61,11 @@ class ConfigurablePlus(Configurable):
 
 
 class StrictArgParse(KVArgParseConfigLoader):
+    """Strict config loader for argparse.
+
+    Will raise errors on unrecognized alias or configuration key.
+    So ``--Unexisting.param=1`` will raise.
+    """
 
     def _handle_unrecognized_alias(self, arg: str):
         self.parser.error(f'Unrecognized alias: {arg}')
@@ -77,3 +97,186 @@ class StrictArgParse(KVArgParseConfigLoader):
         check_config(config, 'c')
         return config
 
+
+class BaseApp(Application):
+    """Base application with some additional features.
+
+
+    """
+    classes = []
+    """List of Configurable classes to manage.
+
+    Define yours and put them here.
+    """
+
+    add_to_aliases: list[type[Configurable]] = []
+    """Automatically add aliases for traits from those Configurable.
+
+    Aliases are the names of the traits. For instance
+    ``--Parameters.threshold=5`` can be set directly as ``--threshold=5``.
+    """
+
+    strict_parsing = Bool(
+        True, help=('If true, raise errors when encountering unknown '
+                    'arguments or configuration keys. Else only prints '
+                    'a warning.')
+    ).tag(config=True)
+
+    config_file = Unicode(
+        'config.py', help='Load this config file.'
+    ).tag(config=True)
+
+    init_config = Bool(
+        False, help='If True, write configuration to file.'
+    )
+
+    flags = {
+        'init-config': ({'BaseApp': {'init_config': True}}, init_config.help)
+    }
+
+    def __init_subclass__(cls, /, **kwargs) -> None:
+        """Subclass init hook.
+
+        Any subclass will automatically run this after being defined.
+        It adds aliases for all traits of the configurable listed
+        in the add_to_aliases class attribute.
+        """
+        super().__init_subclass__(**kwargs)
+        for cfg in cls.add_to_aliases:
+            for name, trait in cfg.class_traits(config=True).items():
+                if name not in cls.aliases:
+                    cls.aliases[name] = (f'{cfg.__name__}.{name}', trait.help)
+
+    def initialize(self, argv=None, ignore_cli: bool = False):
+        """Initialize application.
+
+        - Populate ``config`` attribute with defaults as defined in the
+        various Configurable objects.
+        - Parse command line arguments.
+        - Load configuration file.
+
+        Parameters
+        ----------
+        argv:
+            If not None override command line arguments.
+        ignore_cli:
+            If True, do not parse command line arguments. Useful
+            for jupyter notebooks for instance.
+        """
+        # Initialize defaults defined in Configurable classes
+        self.setup_defaults()
+
+        # First parse CLI (for help for instance, or specify the config files)
+        if not ignore_cli:
+            self.parse_command_line(argv)
+
+        # Read config files (done last but hasn't priority over CLI)
+        if self.config_file:
+            self.load_config_file(self.config_file)
+
+    def start(self):
+        """Start application or subcommands."""
+        super().start()
+        if self.init_config:
+            self.write_config()
+
+    def setup_defaults(self) -> None:
+        """Populate self.config recursively for registered classes.
+
+        Use the defaults values of traits. Traitlets does not do
+        this by default (it implies to walk the nested tree of traits,
+        and traitlets allows to keep things lazy).
+
+        We do this to centralize the configuration, instead of
+        relying on scattered Configurable instances.
+        """
+        def populate(cls: type[Configurable],
+                     config: Config, key: str):
+            subconfig = config.setdefault(key, Config())
+            for name, value in cls().trait_values(config=True).items():
+                # trait name starts with capital > nested config
+                if _is_section_key(name):
+                    populate(value, subconfig, name)
+                else:
+                    subconfig[name] = value
+
+        self.config = Config()
+        for cls in self.classes:
+            populate(cls, self.config, cls.__name__)
+        # save defaults
+        self.config_defaults = copy.deepcopy(self.config)
+
+    def write_config(self, filename: str | None = None,
+                     ask_overwrite: bool = True):
+        """(Over)write a configuration file.
+
+        Parameters
+        ----------
+        filename:
+            Write to this file. If None, the current value of
+            :attr:`config_file` is used.
+        ask_overwrite:
+            If True (default), ask for confirmation if config file
+            already exists. Else, overwrite the file without questions.
+        """
+        if filename is None:
+            filename = self.config_file
+
+        filename = path.realpath(filename)
+
+        if path.exists(filename) and ask_overwrite:
+            print(f"Config file already exists '{filename}")
+            def ask():
+                prompt = 'Overwrite with new config? [y/N]'
+                try:
+                    return input(prompt).lower() or 'n'
+                except KeyboardInterrupt:
+                    print('')  # empty line
+                    return 'n'
+
+            answer = ask()
+            while not answer.startswith(('y', 'n')):
+                print("Please answer 'yes' or 'no'")
+                answer = ask()
+            if answer.startswith('n'):
+                return
+
+        lines = self.generate_config_file().splitlines()
+        # Remove trailing whitespace
+        lines = [line.rstrip() for line in lines]
+
+        with open(filename, 'w') as f:
+            f.write('\n'.join(lines))
+
+    def _create_loader(self, argv, aliases, flags, classes):
+        """Use a strict loader if specified.
+
+        Strict loader is :class:`StrictArgParse`, otherwise the
+        default :class:`KVArgParseConfigLoader` is used.
+        """
+        if self.strict_parsing:
+            loader = StrictArgParse
+        else:
+            loader = KVArgParseConfigLoader
+        return loader(
+            argv, aliases, flags, classes=classes,
+            log=self.log, subcommands=self.subcommands
+        )
+
+    def _classes_inc_parents(self, classes=None):
+        """Iterate through configurable classes, including configurable parents.
+
+        :param classes:
+            The list of classes to iterate; if not set, uses :attr:`classes`.
+
+        Children should always be after parents, and each class should only be
+        yielded once.
+
+        Overwritten to avoid having the :class:`traitlets.Application` parent
+        class documented. I don't think users should change those option
+        globally (or at least not in the context of this library).
+        """
+        for c in super()._classes_inc_parents(classes):
+            if c == Application:
+                continue
+            yield c
