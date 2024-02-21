68699f371b8f8028f3258d86cac58d2361182eb6
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2024-02-22 00:50:53 +0100

    Move get_trait_typehint and account allow_none
    If allow_none is true, we kinda need to typeset trait as if
    it is Union[trait, None]
    We could recurse in some way but we would need to change the value
    allow_none to avoid infinite recursion it's messy.
    I added a sort of hook function that runs before returning the result.
    I previously already hesitated on creating one for various purposes so I
    guess it can be useful.

--- src/autodoc_trait.py
+++ src/autodoc_trait.py
@@ -15,101 +15,14 @@ from sphinx.ext.autodoc import (
     Documenter,
 )
 from traitlets import (
-    Container,
     Dict,
     Enum,
-    Instance,
     List,
     Set,
     TraitType,
-    Tuple,
-    Type,
-    Union,
 )
 
-
-def get_trait_typehint(trait: Any, mode: str = "short") -> str:
-    """Return the typehint corresponding to a trait object.
-
-    Parameters
-    ----------
-    trait:
-        Trait instance. Also accept any other type of object.
-    mode:
-        If "short", add a ~ in front of the full typehint to only print the type name
-        but still have a link.
-    """
-
-    def serialize(obj: Any) -> str:
-        """Return the full import name of any object or type."""
-        if isinstance(obj, type):
-            cls = obj
-        else:
-            cls = obj.__class__
-        name = cls.__name__
-        module = cls.__module__
-        out = "~" if mode == "short" else ""
-        out += f"{module}.{name}"
-        return out
-
-    def recurse(obj):
-        """Recurse this function, keeping optional arguments."""
-        return get_trait_typehint(obj, mode)
-
-    # If simply an object, nothing specific to do:
-    if not isinstance(trait, TraitType):
-        return serialize(trait)
-
-    # Get the typehint of the trait itself
-    typehint = serialize(trait)
-    # ... but it may be a container of some sort
-
-    if isinstance(trait, Union):
-        interior = " | ".join(recurse(subtrait) for subtrait in trait.trait_types)
-        return interior
-
-    # Dict can have either its keys or values TraitType defined (or both).
-    # If missing, automatically set to Any, which we ignore in typehint.
-    # Except if value is defined, then we print Dict[Any, SomeType]
-    if isinstance(trait, Dict):
-        key_val = [""]
-        has_key = trait._key_trait is not None
-        has_val = trait._value_trait is not None
-
-        if has_key:
-            key_val[0] = recurse(trait._key_trait)
-        if has_val:
-            key_val.append(recurse(trait._value_trait))
-            if not has_key:
-                key_val[0] = "~typing.Any"
-
-        if any(key_val):
-            interior = f"[{', '.join(key_val)}]"
-        else:
-            interior = ""
-
-        return f"{typehint}{interior}"
-
-    # Tuple might have any number of traits defined from 0 (Any, not printed)
-    if isinstance(trait, Tuple):
-        interior = ", ".join(recurse(t) for t in trait._traits)
-        if interior:
-            interior = f"[{interior}]"
-        return f"{typehint}{interior}"
-
-    # List and Set
-    if isinstance(trait, Container):
-        interior = recurse(trait._trait)
-        return f"{typehint}[{interior}]"
-
-    if isinstance(trait, Type | Instance):
-        if isinstance(trait.klass, str):
-            interior = trait.klass
-        else:
-            interior = recurse(trait.klass)
-        return f"{typehint}[{interior}]"
-
-    return typehint
+from data_assistant.config.util import get_trait_typehint
 
 
 def stringify(obj) -> str:
@@ -129,7 +42,15 @@ def stringify(obj) -> str:
             fullname = str(obj)
         return f":class:`~{fullname}`"
 
-    return f"``{str(obj)}``"
+    out = str(obj)
+
+    # arbitrary length of characters
+    maxlength = 32
+    if len(out) > maxlength:
+        # the repr/str is too long
+        out = out[:maxlength] + "..."
+
+    return f"``{out}``"
 
 
 class TraitDocumenter(AttributeDocumenter):
--- src/data_assistant/config/util.py
+++ src/data_assistant/config/util.py
@@ -1,6 +1,9 @@
 from collections.abc import Callable
 
 from traitlets.config import Configurable
+from traitlets.traitlets import TraitType, Union, Dict, Tuple, Container, Type, Instance
+
+from typing import Any
 
 
 def tag_all_traits(**metadata) -> Callable:
@@ -18,3 +21,100 @@ def tag_all_traits(**metadata) -> Callable:
         return cls
 
     return decorator
+
+
+def get_trait_typehint(trait: Any, mode: str = "short") -> str:
+    """Return the typehint corresponding to a trait object.
+
+    Parameters
+    ----------
+    trait:
+        Trait instance. Also accept any other type of object.
+    mode:
+        If "short", add a ~ in front of the full typehint to only print the type name
+        but still have a link.
+    """
+
+    def serialize(obj: Any) -> str:
+        """Return the full import name of any object or type."""
+        if isinstance(obj, type):
+            cls = obj
+        else:
+            cls = obj.__class__
+        name = cls.__name__
+        module = cls.__module__
+
+        # fullname (default)
+        out = f"{module}.{name}"
+        # if short we add tilde
+        if mode == "short":
+            out = f"~{out}"
+        # if minimal only keep the name
+        elif mode == "minimal":
+            out = name
+
+        return out
+
+    def recurse(obj):
+        """Recurse this function, keeping optional arguments."""
+        return get_trait_typehint(obj, mode)
+
+    def output(typehint):
+        """Hook before returning the typehint."""
+        if trait.allow_none:
+            typehint += " | None"
+        return typehint
+
+    # If simply an object, nothing specific to do:
+    if not isinstance(trait, TraitType):
+        return serialize(trait)
+
+    # Get the typehint of the trait itself
+    typehint = serialize(trait)
+
+    if isinstance(trait, Union):
+        interior = " | ".join(recurse(subtrait) for subtrait in trait.trait_types)
+        return output(interior)
+
+    # Dict can have either its keys or values TraitType defined (or both).
+    # If missing, automatically set to Any, which we ignore in typehint.
+    # Except if value is defined, then we print Dict[Any, SomeType]
+    if isinstance(trait, Dict):
+        key_val = [""]
+        has_key = trait._key_trait is not None
+        has_val = trait._value_trait is not None
+
+        if has_key:
+            key_val[0] = recurse(trait._key_trait)
+        if has_val:
+            key_val.append(recurse(trait._value_trait))
+            if not has_key:
+                key_val[0] = "~typing.Any"
+
+        if any(key_val):
+            interior = f"[{', '.join(key_val)}]"
+        else:
+            interior = ""
+
+        return output(f"{typehint}{interior}")
+
+    # Tuple might have any number of traits defined from 0 (Any, not printed)
+    if isinstance(trait, Tuple):
+        interior = ", ".join(recurse(t) for t in trait._traits)
+        if interior:
+            interior = f"[{interior}]"
+        return output(f"{typehint}{interior}")
+
+    # List and Set
+    if isinstance(trait, Container):
+        interior = recurse(trait._trait)
+        return output(f"{typehint}[{interior}]")
+
+    if isinstance(trait, Type | Instance):
+        if isinstance(trait.klass, str):
+            interior = trait.klass
+        else:
+            interior = recurse(trait.klass)
+        return output(f"{typehint}[{interior}]")
+
+    return output(typehint)
