1e28b2eeddfde3ac495f529a49ac2bed6eb92ca8
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2024-03-25 11:40:04 +0100

    Fill __all__

--- src/data_assistant/config/__init__.py
+++ src/data_assistant/config/__init__.py
@@ -1,5 +1,36 @@
+"""Specify parameters in a configuration tree."""
+
 from .application import ApplicationBase
 from .dask_config import DaskConfig
+from .loader import (
+    CLILoader,
+    ConfigLoader,
+    ConfigValue,
+    FileLoader,
+    PyLoader,
+    TomlkitLoader,
+    YamlLoader,
+    to_dict,
+    to_nested_dict,
+)
 from .scheme import Scheme, subscheme
+from .util import FixableTrait, RangeTrait, tag_all_traits
 
-__all__ = ["ApplicationBase", "DaskConfig", "Scheme", "subscheme"]
+__all__ = [
+    "ApplicationBase",
+    "CLILoader",
+    "ConfigLoader",
+    "ConfigValue",
+    "DaskConfig",
+    "FileLoader",
+    "FixableTrait",
+    "PyLoader",
+    "RangeTrait",
+    "Scheme",
+    "TomlkitLoader",
+    "YamlLoader",
+    "subscheme",
+    "tag_all_traits",
+    "to_dict",
+    "to_nested_dict",
+]
--- src/data_assistant/data/__init__.py
+++ src/data_assistant/data/__init__.py
@@ -1,6 +1,21 @@
 """Define easily classes to deal with your many datasets."""
 
 from .data_manager import DataManagerBase
+from .loader import LoaderPluginAbstract
+from .plugin import CachePlugin, Plugin, autocached
 from .register import DatasetStore
+from .source import GlobPlugin, MultiFilePluginAbstract
+from .writer import WriterMultiFileAbstract, WriterPluginAbstract
 
-__all__ = ["DataManagerBase", "DatasetStore"]
+__all__ = [
+    "CachePlugin",
+    "DataManagerBase",
+    "DatasetStore",
+    "GlobPlugin",
+    "LoaderPluginAbstract",
+    "MultiFilePluginAbstract",
+    "Plugin",
+    "WriterMultiFileAbstract",
+    "WriterPluginAbstract",
+    "autocached",
+]
