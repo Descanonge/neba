daefce38b80452e9f5c6c99feef60bb82d1e2a18
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2024-10-04 22:00:15 +0200

    Add trait to generator function
    to generate value easily from already created traits

--- tests/trait_generation.py
+++ tests/trait_generation.py
@@ -1,3 +1,16 @@
+"""Generate traits.
+
+We use trait generators objects. They can generate a trait on their own. For composed
+traits (example a List of ...) this is a bit more difficult. In some cases we can
+easily get non-sensical or problematic traits (Union typically). To ease things any
+trait generator that needs inner(s) trait(s) should receive correpsonding generator(s)
+at initialization. That means we must create by hand the recursive organization of
+composed traits, to help a bit.
+
+The module contains generator for all basic traits. And functions to generate traits
+out of thin air, or obtain generator from existing traits.
+"""
+
 import typing as t
 from collections import abc
 
@@ -22,16 +35,16 @@ from tests.util import Drawer, T_Trait
 
 
 class TraitGenerator(t.Generic[T_Trait]):
-    """Generate a trait, hold some info about it.
+    """Generate a trait and valid values.
 
-    Allows to get a strategy for values that should validate.
     This class is the base for every trait type.
 
     Parameters
     ----------
-    has_default
-        Wether to draw a default value (true) or not. If left to None, its value will be
-        picked at random by hypothesis.
+    draw_default
+        Wether to draw a default value (true) or not.
+    allow_none
+        Same as trait parameter. If True, the value strategy can draw None.
     kwargs
         Will be passed when instanciating trait.
     """
@@ -39,9 +52,7 @@ class TraitGenerator(t.Generic[T_Trait]):
     traittype: type[T_Trait]
     """Type of the trait to generate. Each class generates one type."""
 
-    def __init__(
-        self, draw_default: bool = True, allow_none: bool = False, *args, **kwargs
-    ):
+    def __init__(self, draw_default: bool = True, allow_none: bool = False, **kwargs):
         self.draw_default = draw_default
         self.allow_none = allow_none
         self.kwargs = kwargs
@@ -63,7 +74,10 @@ class TraitGenerator(t.Generic[T_Trait]):
         return self._st_value()
 
     def st_default(self) -> st.SearchStrategy:
-        """Return a strategy to pick an appropriate default value."""
+        """Return a strategy to pick an appropriate default value.
+
+        By default this is the same as :meth:`st_value`.
+        """
         return self.st_value()
 
     def draw_pre_instance(self, draw: Drawer, **kwargs) -> dict[str, t.Any]:
@@ -84,44 +98,36 @@ class TraitGenerator(t.Generic[T_Trait]):
         return self.traittype(**kwargs)
 
 
-TGen = t.TypeVar("TGen", bound=type[TraitGenerator])
-
-
-registry: dict[str, type[TraitGenerator]] = {}
-
-
-def register(cls: TGen) -> TGen:
-    key = str(cls)
-    registry[key] = cls
-    return cls
-
-
-@register
 class BoolGen(TraitGenerator[Bool]):
+    """Bool Generator."""
+
     traittype = Bool
 
     def _st_value(self) -> st.SearchStrategy[bool]:
         return st.booleans()
 
 
-@register
 class IntGen(TraitGenerator[Int]):
+    """Integer Generator."""
+
     traittype = Int
 
     def _st_value(self) -> st.SearchStrategy[int]:
         return st.integers()
 
 
-@register
 class FloatGen(TraitGenerator[Float]):
+    """Float Generator."""
+
     traittype = Float
 
     def _st_value(self) -> st.SearchStrategy[float]:
         return st.floats(allow_nan=False, allow_infinity=False)
 
 
-@register
 class UnicodeGen(TraitGenerator[Unicode]):
+    """Unicode Generator."""
+
     traittype = Unicode
 
     def _st_value(self) -> st.SearchStrategy[str]:
@@ -129,6 +135,11 @@ class UnicodeGen(TraitGenerator[Unicode]):
 
 
 class EnumGen(TraitGenerator[Enum]):
+    """Enum Generator.
+
+    Values must be supplied for the Enum.
+    """
+
     traittype = Enum
 
     def __init__(self, values: abc.Sequence[t.Any], **kwargs):
@@ -144,10 +155,18 @@ class EnumGen(TraitGenerator[Enum]):
 
 
 class ComposedGenerator(TraitGenerator[T_Trait]):
-    pass
+    """Generator with inner(s) trait(s).
+
+    No features, mainly for type checking.
+    """
 
 
 class ContainerGen(ComposedGenerator[T_Trait]):
+    """Single inner trait generator (List and Set).
+
+    A generator must be supplied for the inner trait.
+    """
+
     def __init__(self, inner_gen: TraitGenerator, **kwargs):
         super().__init__(**kwargs)
         self.inner_gen = inner_gen
@@ -161,6 +180,8 @@ class ContainerGen(ComposedGenerator[T_Trait]):
 
 
 class ListGen(ContainerGen[List]):
+    """List Generator."""
+
     traittype: type[List] = List
 
     def st_default(self) -> st.SearchStrategy[list]:
@@ -171,6 +192,8 @@ class ListGen(ContainerGen[List]):
 
 
 class SetGen(ContainerGen[Set]):
+    """List Generator."""
+
     traittype = Set
 
     def st_default(self) -> st.SearchStrategy[set]:
@@ -180,15 +203,66 @@ class SetGen(ContainerGen[Set]):
         return st.sets(self.inner_gen.st_value())
 
 
-class InstanceGen(ContainerGen[Instance]):
+class ClassGen(ComposedGenerator[T_Trait]):
+    """Class related Generator (Instance and Type).
+
+    Store the target class.
+    """
+
+    def __init__(self, klass, **kwargs):
+        super().__init__(**kwargs)
+        self.klass = klass
+
+    def get_subclass(self, name: str = "") -> type:
+        """Generate subclass.
+
+        The only base is :attr:`klass`, and no definitions are added.
+
+        Parameters
+        ----------
+        name
+            Name of the subclass. If left empty, "_subclass" will be added to the
+            current class.
+        """
+        if not name:
+            name = self.klass.__name__ + "_subclass"
+        cls = type(name, (self.klass,), {})
+        return cls
+
+
+class InstanceGen(ClassGen[Instance]):
+    """Instance Generator.
+
+    Can return an instance of specified klass or one of a generated subclass.
+    """
+
     traittype: type[Instance] = Instance
 
+    def _st_value(self) -> st.SearchStrategy:
+        return st.sampled_from([self.klass(), self.get_subclass()()])
 
-class TypeGen(ContainerGen[Type]):
+
+class TypeGen(ClassGen[Type]):
+    """Type Generator.
+
+    Does not handle str specifically well. If klass is a string, just return this value.
+    """
+
     traittype = Type
+
+    def _st_value(self) -> st.SearchStrategy[type]:
+        if isinstance(str, self.klass):
+            return st.just(self.klass)
+        # clsname = f"{self.klass.__module__}.{self.klass.__qualname__}"
+        return st.sampled_from([self.klass, self.get_subclass()])
 
 
 class UnionGen(ComposedGenerator[Union]):
+    """Union generator.
+
+    Inner traits must be supplied.
+    """
+
     traittype = Union
 
     def __init__(self, inner_gens: abc.Sequence[TraitGenerator], **kwargs):
@@ -263,7 +337,23 @@ class TupleGen(ComposedGenerator[Tuple]):
 
 
 class DictGen(ComposedGenerator[Dict]):
+    """Dict generator.
+
+    Key and value generator must be supplied.
+    """
+
     traittype = Dict
+
+    def __init__(self, key_gen: TraitGenerator, value_gen: TraitGenerator, **kwargs):
+        super().__init__(**kwargs)
+        self.key_gen = key_gen
+        self.value_gen = value_gen
+
+    def _st_value(self) -> st.SearchStrategy[dict]:
+        return st.dictionaries(self.key_gen.st_value(), self.value_gen.st_value())
+
+
+registered_gens: list[type[TraitGenerator]] = [BoolGen, IntGen, FloatGen, UnicodeGen]
 
 
 def st_trait_gen(**kwargs) -> st.SearchStrategy[TraitGenerator]:
@@ -276,10 +366,101 @@ def st_trait_gen(**kwargs) -> st.SearchStrategy[TraitGenerator]:
 
     @st.composite
     def strat(draw: Drawer):
-        gen_type = draw(st.sampled_from(list(registry.values())))
+        gen_type = draw(st.sampled_from(list(registered_gens)))
         return gen_type(**kwargs)
 
     return strat()
+
+
+TRAIT_TO_GEN: dict[type[TraitType], type[TraitGenerator]] = {
+    Bool: BoolGen,
+    Int: IntGen,
+    Float: FloatGen,
+    Unicode: UnicodeGen,
+    Enum: EnumGen,
+    List: ListGen,
+    Set: SetGen,
+    Dict: DictGen,
+    Tuple: TupleGen,
+    Instance: InstanceGen,
+    Type: TypeGen,
+    Union: UnionGen,
+}
+
+
+def trait_to_gen(
+    trait: T_Trait, allow_none: bool | None = None, **kwargs
+) -> TraitGenerator[T_Trait]:
+    """Return TraitGenerator instance corresponding to trait.
+
+    Use information present in the trait to inform the generators, inner trait types
+    for instance.
+
+    For List and Set, if the inner trait is missing, `Int()` will be used.
+    For Dict, if the key or value traits are missing, `Unicode()` and `Int()` will be
+    used for the keys and values respectively.
+    For Instance and Trrait, if the klass is missing, `object` will be used.
+
+    Kwargs is unused.
+    """
+    if allow_none is None:
+        allow_none = trait.allow_none
+    kw = dict(draw_default=False, allow_none=allow_none)
+
+    cls = type(trait)
+
+    if isinstance(trait, Bool | Int | Float | Unicode):
+        return TRAIT_TO_GEN[cls](**kw)
+
+    if isinstance(trait, Enum):
+        assert trait.values is not None
+        return EnumGen(trait.values, **kw)  # type: ignore[return-value]
+
+    if isinstance(trait, List | Set):
+        inner = trait._trait
+        if inner is None:
+            inner = Int()
+        gen = TRAIT_TO_GEN[cls]
+        assert issubclass(gen, ContainerGen)
+        return gen(trait_to_gen(inner), **kw)
+
+    if isinstance(trait, Dict):
+        key_trait = trait._key_trait
+        val_trait = trait._value_trait
+        if key_trait is None:
+            key_trait = Unicode()
+        if val_trait is None:
+            val_trait = Int()
+        return DictGen(
+            key_gen=trait_to_gen(key_trait, **kw),
+            value_gen=trait_to_gen(val_trait),
+            **kw,
+        )  # type: ignore[return-value]
+
+    if isinstance(trait, Union):
+        return UnionGen(  # type: ignore[return-value]
+            [trait_to_gen(t, **kw) for t in trait.trait_types], **kw
+        )
+
+    if isinstance(trait, Tuple):
+        return TupleGen(  # type: ignore[return-value]
+            *[trait_to_gen(t, **kw) for t in trait._traits], **kw
+        )
+
+    if isinstance(trait, Instance | Type):
+        klass = trait.klass
+        if klass is None:
+            klass = object
+        gen = TRAIT_TO_GEN[cls]
+        assert issubclass(gen, ClassGen)
+        return gen(klass, **kw)
+
+    raise TypeError(f"Cannot find generator for trait type '{type(trait)}'")
+
+
+def trait_to_strat(trait: TraitType, **kwargs) -> st.SearchStrategy:
+    gen = trait_to_gen(trait, **kwargs)
+    return gen.st_value()
 
 
 def st_trait(**kwargs) -> st.SearchStrategy[TraitType]:
