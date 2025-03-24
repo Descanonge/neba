284eed6fba051cbed2f6fba0cbe9682b29e93e7f
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-03-24 19:07:41 +0100

    Revert "Return pytest exit code"
    This reverts commit 32374ed1596f3ec3db3464c2c6ce68f37c8c6141.

--- tests/generic_sections.py
+++ tests/generic_sections.py
@@ -335,8 +335,6 @@ class Empty_b_Info(SectionInfo):
 
 
 class GenericConfigInfo(GenericSectionInfo):
-    # TODO add aliases
-
     section = GenericConfig
     subsections = dict(
         sub_generic=GenericSectionInfo(),
